32639c1e5a7d5f84be83c8776d921e634c7bfbdc
Paula Illanes <illanesrpaula@gmail.com>
2021-02-14 01:27:06 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
 import PrincipalPage from "./screens/PrincipalPage";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
