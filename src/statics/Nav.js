a73537e3e2117c54b164b2b4db97bd5311d86526
Paula Illanes <illanesrpaula@gmail.com>
2021-02-14 01:32:18 -0300

    Deleting react router

--- src/statics/Nav.js
+++ src/statics/Nav.js
@@ -1,20 +1,13 @@
 import React from "react";
 import styles from "./Nav.module.scss";
-import { Link } from "react-router-dom";
 
 const Nav = ({ onClick, cartItems }) => {
   return (
     <nav className={styles.navContainer}>
       <ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/about-us">ABOUT-US</Link>
-        </li>
-        <li>
-          <Link to="/latest">LATEST</Link>
-        </li>
+        <li>HOME</li>
+        <li>ABOUT-US</li>
+        <li>LATEST</li>
       </ul>
       <button onClick={onClick}>CART ({cartItems})</button>
     </nav>
