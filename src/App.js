e45fc48d95ffb44455bdf5d3211839fa4e418870
Paula Illanes <illanesrpaula@gmail.com>
2021-02-14 01:13:41 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.scss";
 import PrincipalPage from "./screens/PrincipalPage";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Route exact path="/">
           <PrincipalPage />
         </Route>
